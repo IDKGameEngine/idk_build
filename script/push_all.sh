7aa4f550e13e1b8e8cd47ca7dc8d6c62f2cc7d0b
mellic03 <mellic03@gmail.com>
2026-03-09 01:27:29 +1000

    Removed push_all.sh

--- script/push_all.sh
+++
@@ -1,7 +0,0 @@
-THIS_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-for path in $THIS_DIR/../../idk_*; do
-    if [ -d "$path" ]; then
-        cd "${path}" && git add . && git commit -m "auto commit" && git push 
-    fi
-done
