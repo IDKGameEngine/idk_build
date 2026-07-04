8d1247b54c28226809abf77790d7d5af4cca1bef
mellic03 <mellic03@gmail.com>
2026-07-05 07:52:41 +1000

    ree

---
+++ script/pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+POLY_DIR=$(cd ${THIS_DIR}/../../ && pwd)
+
+REPO_DIRS=(
+    "${POLY_DIR}/idk_build"
+    "${POLY_DIR}/idk_core"
+    "${POLY_DIR}/idk_engine"
+    "${POLY_DIR}/idk_game"
+    "${POLY_DIR}/idk_gfx"
+)
+
+for DIR in "${REPO_DIRS[@]}"; do
+    echo "cd $DIR && git pull"
+    cd $DIR && git pull
+done
