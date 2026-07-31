f6f871bc6e058d0dfcb91b95145addc6319afe2a
mellic03 <mellic03@gmail.com>
2026-07-31 18:08:11 +1000

    rework: successful compilation + execution

--- script/pull.sh
+++ script/pull.sh
@@ -4,8 +4,8 @@ THIS_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 POLY_DIR=$(cd ${THIS_DIR}/../../ && pwd)
 
 REPO_DIRS=(
+    "${POLY_DIR}/libidk"
     "${POLY_DIR}/idk_build"
-    "${POLY_DIR}/idk_core"
     "${POLY_DIR}/idk_engine"
     "${POLY_DIR}/idk_game"
     "${POLY_DIR}/idk_gfx"
