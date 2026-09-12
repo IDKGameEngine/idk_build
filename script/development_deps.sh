ddfce524fb5249a404b4ee2874b6154a8c3ae1eb
mellic03 <mellic03@gmail.com>
2026-09-12 22:42:29 +1000

    Build changes

--- script/development_deps.sh
+++ script/development_deps.sh
@@ -46,8 +46,8 @@ build_jolt()
 build_slang()
 {
     cd ${REPO_DIR}/submodule/slang
-    git fetch https://github.com/shader-slang/slang.git 'refs/tags/*:refs/tags/*'
     git submodule update --init
+    git fetch https://github.com/shader-slang/slang.git 'refs/tags/*:refs/tags/*'
     cmake --preset default -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
     cmake --build --preset release
     cmake --build . --target install
