7b3484247328bb08d7fee605066b4a507fcd7a88
mellic03 <mellic03@gmail.com>
2026-09-13 21:59:37 +1000

    Removed slang subtree

--- thirdparty/slang/tests/diagnostics/command-line/pass-through-no-stage.hlsl
+++
@@ -1,9 +0,0 @@
-// pass-through-no-stage.hlsl
-
-// Trying to compile in `-pass-through` mode without
-// specifying a stage is an error, because the downstream
-// compilers don't support inferring the stage from
-// an attribute.
-
-//DIAGNOSTIC_TEST:SIMPLE(diag=CHECK):-pass-through fxc -entry main
-//CHECK: no stage was specified for entry point 'main'; when using the '-pass-through' option, stages must be fully specified on the command line
