b439b3f11d5b6eba171efce77ddf88279373db86
mellic03 <mellic03@gmail.com>
2026-09-19 13:52:12 +1000

    Build changes

--- script/local.sh
+++ script/local.sh
@@ -16,51 +16,43 @@ else
     exit 1
 fi
 
-build_repos()
+get_repo()
 {
-    for REPO_NAME in "$@"; do
-        printf "\n\n-------------------------------- Building ${REPO_NAME} --------------------------------\n"
-        build_$REPO_NAME
-    done
-}
-
-clone_repo()
-{
-    REPO_NAME="$1"
-    REPO_BRANCH="$2"
-    REPO_URL="$3"
+    REPO_BRANCH="$1"
+    REPO_URL="$2"
+    REPO_NAME=$(basename "$REPO_URL" .git)
     [ ! -d "${THIRDPARTY_DIR}/${REPO_NAME}" ] && \
         git clone --depth 1 -b ${REPO_BRANCH} "${REPO_URL}" "${THIRDPARTY_DIR}/${REPO_NAME}"
+    echo "${THIRDPARTY_DIR}/${REPO_NAME}"
 }
 
 build_assimp()
 {
-    clone_repo assimp v6.0.5 https://github.com/assimp/assimp
-    cd ${THIRDPARTY_DIR}/assimp
+    cd $(get_repo v6.0.5 https://github.com/assimp/assimp)
 
-    # cd ${THIRDPARTY_DIR}/assimp
-    # cmake CMakeLists.txt \
-    #     -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
-    #     -DCMAKE_BUILD_TYPE=Release \
-    #     -DBUILD_SHARED_LIBS=ON \
-    #     -DASSIMP_NO_EXPORT=ON \
-    #     -DASSIMP_BUILD_TESTS=OFF \
-    #     -DASSIMP_BUILD_ZLIB=ON \
-    #     -DASSIMP_USE_HUNTER=ON
-    # cmake --build . -j $(nproc) && cmake --install .
+    cd ${THIRDPARTY_DIR}/assimp
+    cmake CMakeLists.txt \
+        -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=ON \
+        -DASSIMP_NO_EXPORT=ON \
+        -DASSIMP_BUILD_TESTS=OFF \
+        -DASSIMP_BUILD_ZLIB=ON \
+        -DASSIMP_USE_HUNTER=ON
+    cmake --build . -j $(nproc) && cmake --install .
 }
 
 build_glm()
 {
-    cd ${THIRDPARTY_DIR}/glm
-    cmake -B build . -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
+    cd $(get_repo master https://github.com/g-truc/glm.git)
+    cmake -S . -B build -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DGLM_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF
     cmake --build build -- all
     cmake --build build -- install
 }
 
 build_imgui()
 {
-    cd ${THIRDPARTY_DIR}/imgui
+    cd $(get_repo docking git@github.com:IDKGameEngine/imgui.git)
     cmake -S . -B build -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DCMAKE_BUILD_TYPE=Release
     cmake --build build --config Release
     cmake --install build --config Release
@@ -68,8 +60,7 @@ build_imgui()
 
 build_jolt()
 {
-    clone_repo JoltPhysics v5.6.0 https://github.com/jrouwe/JoltPhysics.git
-    cd ${THIRDPARTY_DIR}/JoltPhysics/Build
+    cd $(get_repo v5.6.0 https://github.com/jrouwe/JoltPhysics.git)/Build
 
     ./cmake_linux_clang_gcc.sh \
         Release g++ \
@@ -84,18 +75,25 @@ build_jolt()
 
 build_steamworks-sdk()
 {
-    clone_repo steamworks-sdk main git@github.com:IDKGameEngine/steamworks-sdk.git
-    cd ${THIRDPARTY_DIR}/steamworks-sdk
+    cd $(get_repo main git@github.com:IDKGameEngine/steamworks-sdk.git)
     cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
     cmake --build build && cmake --install build
 }
 
 build_vulkan-sdk()
 {
-    clone_repo vulkan-sdk main git@github.com:IDKGameEngine/vulkan-sdk.git
-    cd $THIRDPARTY_DIR/vulkan-sdk
+    cd $(get_repo main git@github.com:IDKGameEngine/vulkan-sdk.git)
     cmake -S . -B build -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}"
     cmake --build build && cmake --install build
 }
 
-build_repos assimp # glm imgui jolt steamworks-sdk vulkan-sdk
+
+build_repos()
+{
+    for REPO_NAME in "$@"; do
+        printf "\n\n-------------------------------- Building ${REPO_NAME} --------------------------------\n"
+        build_$REPO_NAME
+    done
+}
+
+build_repos assimp glm imgui jolt steamworks-sdk vulkan-sdk
